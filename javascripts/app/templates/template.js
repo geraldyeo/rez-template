193e3db8b47d2e9236ad8e59bca988d313ac3e4c
Gerald Yeo <gerald.yeo@gmail.com>
2013-02-08 19:26:46 +0800

    wip

--- javascripts/app/templates/template.js
+++ javascripts/app/templates/template.js
@@ -14,6 +14,7 @@ define([
            'text!app/templates/page-main.hbs',
            'text!app/templates/page-about.hbs',
            'text!app/templates/page-tables.hbs',
+           'text!app/templates/index-tables.hbs',
            'text!app/templates/sect-table.hbs',
            'text!app/templates/sect-food.hbs',
            'text!app/templates/partials/table-menu.hbs',
@@ -22,20 +23,21 @@ define([
            'ember'
        ],
 
-       function (app_tpl, mainPage_tpl, aboutPage_tpl, tablesPage_tpl, tableSect_tpl, foodSect_tpl, tableMenu_part) {
+       function (app_tpl, mainPage_tpl, aboutPage_tpl, tablesPage_tpl, tablesIndex_tpl, tableSect_tpl, foodSect_tpl, tableMenu_part) {
            return {
                // pages
-               "application":Ember.Handlebars.compile(app_tpl),
-               "index"      :Ember.Handlebars.compile(mainPage_tpl),
-               "about"      :Ember.Handlebars.compile(aboutPage_tpl),
-               "tables"     :Ember.Handlebars.compile(tablesPage_tpl),
+               "application" :Ember.Handlebars.compile(app_tpl),
+               "index"       :Ember.Handlebars.compile(mainPage_tpl),
+               "about"       :Ember.Handlebars.compile(aboutPage_tpl),
+               "tables"      :Ember.Handlebars.compile(tablesPage_tpl),
 
                // sections
-               "table":Ember.Handlebars.compile(tableSect_tpl),
-               "food" :Ember.Handlebars.compile(foodSect_tpl),
+               "tables/index":Ember.Handlebars.compile(tablesIndex_tpl),
+               "table"       :Ember.Handlebars.compile(tableSect_tpl),
+               "food"        :Ember.Handlebars.compile(foodSect_tpl),
 
                // partials
-               "_tableMenu":Ember.Handlebars.compile(tableMenu_part)
+               "_tableMenu"  :Ember.Handlebars.compile(tableMenu_part)
            };
        }
 );
